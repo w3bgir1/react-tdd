d158f325751354284f8bc9b0cecf19a3c72efd44
Oleksandra Akulshyna <oleksandraakulshyna@anyhost.students3.wls.int.nnbs.nyenrode.nl>
2019-10-14 17:07:27 +0200

    in progress

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
 class Gift extends Component {
   constructor() {
     super();
+    this.state = {
+      person: "",
+      present: ""
+    };
   }
 
   render() {
     return (
       <div>
-        wemfke
-        <p>dekf</p>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              className="input-person"
+              onChange={e =>
+                this.setState({
+                  person: e.target.value
+                })
+              }
+            />
+            <Form.Label>Person</Form.Label>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              className="input-present"
+              onChange={e =>
+                this.setState({
+                  present: e.target.value
+                })
+              }
+            />
+            <Form.Label>Present</Form.Label>
+          </Form.Group>
+          {/* <Button>Add</Button> */}
+        </Form>
       </div>
     );
   }
