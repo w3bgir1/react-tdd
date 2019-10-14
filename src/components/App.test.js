7f364a8868ca886d3cc09fb59fdd04eee30f4585
Oleksandra Akulshyna <oleksandraakulshyna@anyhost.students3.wls.int.nnbs.nyenrode.nl>
2019-10-14 16:08:02 +0200

    init

---
+++ src/components/App.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+describe("App", () => {
+  const app = shallow(<App />);
+
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it("initializes state with empty list", () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+
+  describe("when clicking the `add gift` button", () => {
+    beforeEach(() => {
+      app.find(".btn-add").simulate("click");
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it("adds a new gift to `state`", () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    it("adds a new gift to the rendered list", () => {
+      expect(app.find(".gift-list").children().length).toEqual(1);
+    });
+  });
+});
---
+++ src/components/Gift.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Gift extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        wemfke
+        <p>dekf</p>
+      </div>
+    );
+  }
+}
+
+export default Gift;
